0061239fba6eefe22304b4ebf5044f61ed1641df
Oliver Santiago <oliveratwork08@gmail.com>
2024-07-31 17:05:53 -0700

    switch to image with title

--- src/components/PortalRow/index.js
+++ src/components/PortalRow/index.js
@@ -21,13 +21,13 @@ class PortalRow extends HTMLElement {
 
       if (isFull) {
         entity.title = item.text.title.full.program.default.content;
-        entity.background = item.image.hero_tile["1.78"].program.default.url;
+        entity.background = item.image.tile["1.78"].program.default.url;
         entity.titleLayered = item.image.title_treatment["1.78"].program.default.url;
       }
 
       if (isSeries) {
         entity.title = item.text.title.full.series.default.content;
-        entity.background = item.image.hero_tile["1.78"].series.default.url;
+        entity.background = item.image.tile["1.78"].series.default.url;
         entity.titleLayered = item.image.title_treatment["1.78"].series.default.url;
       }
 
